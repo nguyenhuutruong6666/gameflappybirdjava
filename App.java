19df6ca84d71ca85bf8f53320e970dd39a10393f
manhtri <nmanhtri07@gmail.com>
2025-02-08 16:25:02 +0700

    testduthucngo

--- App.java
+++ App.java
@@ -4,10 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
         int boardHeight = 640;
-        // manhtgringoo
+        // duthucngo
         JFrame frame = new JFrame("Flappy Bird");
         // frame.setVisible(true);
-		frame.setSize(boardWidth, boardHeight);
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,6 +17,6 @@ public class App {
         frame.add(menuScreen);
         frame.pack();
         frame.setVisible(true);
-        
+
     }
 }
