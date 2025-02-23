066a9a8c710206e7d2fc6882aed6fac4338c5579
cngthuc <congthuc0492004@gmail.com>
2025-02-24 00:09:07 +0700

    back về menu ở class Information

--- Information.java
+++ Information.java
@@ -1,26 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Information extends JPanel {
     private JFrame frame;
     private Image backgroundImg;
     private Image qrImage;
+    private JButton backButton;
 
     public Information(JFrame frame) {
         this.frame = frame;
         this.setPreferredSize(new Dimension(360, 640));
+        setLayout(null); // Dùng layout null để đặt vị trí nút tự do
 
+        // Tải hình ảnh
         backgroundImg = new ImageIcon(getClass().getResource("/picture/flappybirdbg.png")).getImage();
         qrImage = new ImageIcon(getClass().getResource("/picture/maqr.png")).getImage();
+
+        // Tạo nút "Back to Menu"
+        backButton = new JButton("Back to Menu");
+        backButton.setFont(new Font("Arial", Font.BOLD, 15));
+        backButton.setFocusPainted(false);
+        backButton.setBounds(100, 500, 160, 40); // Đặt vị trí nút
+        backButton.setBackground(Color.RED);
+        backButton.setForeground(Color.WHITE);
+
+        // Sự kiện bấm nút
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                returnToMenu();
+            }
+        });
+
+        // Thêm nút vào panel
+        this.add(backButton);
+    }
+
+    private void returnToMenu() {
+        frame.getContentPane().removeAll();
+        MenuScreen menuScreen = new MenuScreen(frame);
+        frame.add(menuScreen);
+        frame.pack();
+        frame.revalidate();
+        frame.repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImg, 0, 0, getWidth(), getHeight(), this);
-        
+
         g.setFont(new Font("Arial", Font.BOLD, 24));
-        g.setColor(Color.black);
+        g.setColor(Color.BLACK);
         String text = "Donate in here";
         FontMetrics metrics = g.getFontMetrics();
         int x = (getWidth() - metrics.stringWidth(text)) / 2;
